39866454b7bd57d1cccd6bd8cfd15d7b624f83f8
thisisaaronland <thisisaaronland@localhost>
2023-04-17 15:57:31 -0700

    first basically working version

--- README.md
+++ README.md
@@ -1,10 +1,98 @@
-# rs-bcbp-wasm
+# rs-sfomuseum-bcbp
 
-Rust-based WebAssembly (WASM) binary wrapping the `martinmroz/iata_bcbp` crate.
+SFO Museum-specific Rust-based WebAssembly (WASM) binary wrapping the `martinmroz/iata_bcbp` crate.
 
 ## Important
 
 This is both a work-in-progress and a learning exercise to get familiar with Rust.
+
+It does _not_ return all of the data produced by the `martinmroz/iata_bcbp` crate but rather a list of truncated data for each leg of a flight.
+
+## Example
+
+```
+$> make wasm
+wasm-pack build --target web
+[INFO]: 🎯  Checking for the Wasm target...
+[INFO]: 🌀  Compiling to Wasm...
+   Compiling rs-sfomuseum-bcbp v0.1.0 (/usr/local/sfomuseum/rs-bcbp-wasm)
+    Finished release [optimized] target(s) in 0.42s
+[INFO]: ⬇️  Installing wasm-bindgen...
+[INFO]: found wasm-opt at "/usr/local/bin/wasm-opt"
+[INFO]: Optimizing wasm binaries with `wasm-opt`...
+[INFO]: ✨   Done in 1.42s
+[INFO]: 📦   Your wasm pkg is ready to publish at /usr/local/sfomuseum/rs-bcbp-wasm/pkg.
+```
+
+And then load the WASM binary in a webpage like this:
+
+```
+<html>
+    <head>
+	<title>barcode</title>
+    </head>
+    <body>
+	<form>
+	    <input type="text" id="barcode" disabled="disabled" />
+	    <button type="submit" id="submit" disabled="disabled">Scan</button>
+	</form>
+	<ul id="legs">
+	</ul>
+    </body>
+    <script type="module">
+
+     import init, { parse } from './rs_sfomuseum_bcbp.js';
+
+     async function main() {
+            await init();
+
+	 var barcode_el = document.getElementById("barcode");	    
+	 var submit_el = document.getElementById("submit");
+	 var legs_el = document.getElementById("legs");	 
+
+	 barcode_el.removeAttribute("disabled");	    
+	 submit_el.removeAttribute("disabled");
+	 
+	 submit_el.onclick = function(e){
+
+	     legs_el.innerHTML = "";
+
+	     try {
+		 
+		 let raw = barcode.value;
+		 let rsp = parse(raw);
+		 let count = rsp.length;
+	     
+		 for (var i=0; i < count; i++){
+		     
+		     var item = document.createElement("li");
+		     item.appendChild(document.createTextNode(rsp[i].string()));
+		 }
+		 
+		 legs_el.appendChild(item);
+		 
+	     } catch(err){
+
+		 var item = document.createElement("li");
+		 item.appendChild(document.createTextNode(err));
+		 legs_el.appendChild(item);		 
+		 console.log(err);
+	     };
+	     
+	     return false;
+	 };
+	 
+     }
+     
+     main();
+
+    </script>
+</html>
+```
+
+And then if you load that web page in a browser and parse the string `M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100` you should see something like this:
+
+![](docs/image/rs-sfomuseum-bcbp.png)
 
 ## See also
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 extern crate iata_bcbp;
 
-use iata_bcbp::Bcbp;
-use wasm_bindgen::prelude::*;
-use std::str::FromStr;
 use arrayvec::ArrayVec;
+use iata_bcbp::Bcbp;
 use js_sys::Array;
+use std::str::FromStr;
+use wasm_bindgen::prelude::*;
 
 const MAX_LEGS: usize = 10;
-    
+
+// https://github.com/martinmroz/iata_bcbp/blob/master/src/bcbp/mod.rs
+
 #[wasm_bindgen]
 pub struct Leg {
     carrier: String,
@@ -19,6 +21,16 @@ pub struct Leg {
 
 #[wasm_bindgen]
 impl Leg {
+    pub fn string(&self) -> String {
+        return format!(
+            "{} #{} {} -> {} {}",
+            self.carrier(),
+            self.flightnumber(),
+            self.origin(),
+            self.destination(),
+            self.date()
+        );
+    }
 
     pub fn carrier(&self) -> String {
         self.carrier.clone()
@@ -39,37 +51,40 @@ impl Leg {
     pub fn date(&self) -> String {
         self.date.clone()
     }
-
 }
 
 #[wasm_bindgen]
-pub fn parse(barcode: &str) -> Array {
+pub fn parse(barcode: &str) -> Result<Array, JsValue> {
+    let rsp = Bcbp::from_str(barcode);
 
-    let rsp = Bcbp::from_str(barcode).unwrap();
+    let _ = match rsp {
+        Ok(bcbp_data) => {
+            let mut legs = ArrayVec::<[Leg; MAX_LEGS]>::new();
+            let mut i: usize = 0;
 
-    let mut legs = ArrayVec::<[Leg; MAX_LEGS]>::new();
+            for leg in bcbp_data.legs().iter() {
+                let l: Leg = Leg {
+                    carrier: leg.operating_carrier_designator().trim().to_string(),
+                    flightnumber: leg.flight_number().trim().to_string(),
+                    origin: leg.from_city_airport_code().trim().to_string(),
+                    destination: leg.to_city_airport_code().trim().to_string(),
+                    date: leg.date_of_flight().trim().to_string(),
+                };
 
+                legs.push(l);
 
-    let mut i: usize = 0;
-    
-    for leg in rsp.legs().iter() {
+                i = i + 1;
 
-    	let l: Leg = Leg{
-	    carrier: leg.operating_carrier_designator().trim().to_string(),
-	    flightnumber: leg.flight_number().trim().to_string(),
-	    origin: leg.from_city_airport_code().trim().to_string(),
-	    destination: leg.to_city_airport_code().trim().to_string(),
-	    date: leg.date_of_flight().trim().to_string(),	    
-	};
+                if i >= MAX_LEGS {
+                    break;
+                }
+            }
 
-	legs.push(l);
-
-	i = i + 1;
-
-	if i >= MAX_LEGS {
-	   break;
-	}	
-    }
-
-    legs.into_iter().map(JsValue::from).collect()
+            return Ok(legs.into_iter().map(JsValue::from).collect());
+        }
+        Err(err) => {
+            // https://github.com/martinmroz/iata_bcbp/blob/master/src/error.rs
+            return Err(format!("Failed to parse barcode, {}", err).into());
+        }
+    };
 }
